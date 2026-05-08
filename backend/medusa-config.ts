4fea24401723882f57543945ee27f0a4045ba0f5
santi1999 <ssaldivar1@fordham.edu>
2026-05-07 20:29:35 -0400

    adjusting some configuration deployment files

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -13,15 +13,22 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET!,
       cookieSecret: process.env.COOKIE_SECRET!,
     },
-    databaseDriverOptions: { ssl: false },
+    databaseDriverOptions: { ssl: false, sslmode: "disable" },
+    workerMode: process.env.WORKER_MODE as "shared" | "worker" | "server",
   },
   admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
     vite: (config) => {
       if (process.env.NODE_ENV === "production") return config
       return {
         server: {
           host: "0.0.0.0",
-          allowedHosts: ["localhost", ".localhost", "127.0.0.1", "pitayanewyork.com"],
+          allowedHosts: [
+            "localhost",
+            ".localhost",
+            "127.0.0.1",
+            "pitayanewyork.com",
+          ],
           hmr: {
             port: 5173,
             clientPort: 5173,
@@ -40,6 +47,52 @@ module.exports = defineConfig({
   //           id: "stripe",
   //           options: {
   //             apiKey: process.env.STRIPE_API_KEY,
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     resolve: "@medusajs/medusa/caching",
+  //     options: {
+  //       providers: [
+  //         {
+  //           resolve: "@medusajs/caching-redis",
+  //           id: "caching-redis",
+  //           is_default: true,
+  //           options: {
+  //             redisUrl: process.env.CACHE_REDIS_URL,
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     resolve: "@medusajs/medusa/event-bus-redis",
+  //     options: {
+  //       redisUrl: process.env.REDIS_URL,
+  //     },
+  //   },
+  //   {
+  //     resolve: "@medusajs/medusa/workflow-engine-redis",
+  //     options: {
+  //       redis: {
+  //         // Note: This was `url` before v2.12.2
+  //         // It's now deprecated in favor of `redisUrl`
+  //         redisUrl: process.env.REDIS_URL,
+  //       },
+  //     },
+  //   },
+  //   {
+  //     resolve: "@medusajs/medusa/locking",
+  //     options: {
+  //       providers: [
+  //         {
+  //           resolve: "@medusajs/medusa/locking-redis",
+  //           id: "locking-redis",
+  //           is_default: true,
+  //           options: {
+  //             redisUrl: process.env.LOCKING_REDIS_URL,
   //           },
   //         },
   //       ],
